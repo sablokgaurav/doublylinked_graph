cdc0c7ccd6b1c716c7bd5722e16abe8ce562ff94
Gaurav Sablok <sablokg@gmail.com>
2024-03-15 11:44:08 +0100

    Update nodecal.jl

--- nodecal.jl
+++ nodecal.jl
@@ -1,6 +1,10 @@
 # Universitat Potsdam
 # Author Gaurav Sablok
 # Date 2024-3-15
+using Pkg
+Pkg.add("Networks.jl")
+Pkg.add("Filesystem.jl")
+Pkg.add("DataStructures.jl")
 struct nodeparse {
   node: String
   nodeconnection: float64
@@ -20,6 +24,15 @@ nodeadd = Matrix{Int64[Float64]}
 for i in 1:length(nodeparse)
     for j in 1:length(matrixcal)
       iternode = nodeparse[i]*matrixcal[j]
-        push!(nodeaadd,nodeparse)
-        
+        push!(nodeaadd,iternode)
+# method addition fot he plotting the nodes
+# method addition for parsing the gff and also the gfa nodes.
+for i in 1:length(nodeparse)
+    for j in 1:length(matrixcal)
+        # implementing a heap interaction
+            if nodeparse[i] < nodeparse[i+1]
+            binaryheapnode = Binaryheap{Union{Int64, Float64}}
+            push!(binaryheapnode, nodeparse[i])
+              
+              
         
