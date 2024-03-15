a72edebc4ffeabf03099bc7c2ab5ef2000a926be
Gaurav Sablok <sablokg@gmail.com>
2024-03-15 11:04:19 +0100

    Create nodecal.jl

---
+++ nodecal.jl
@@ -0,0 +1,13 @@
+# Universitat Potsdam
+# Author Gaurav Sablok
+# Date 2024-3-15
+struct nodeparse {
+  node: String
+  nodeconnection: float64
+  next:Int64
+  storingarray: Vector{Int64}
+}
+  # add a nodeparse struct
+  # add a visualization struct
+  # add a reading from a gtf or the gff file
+nodeparse
